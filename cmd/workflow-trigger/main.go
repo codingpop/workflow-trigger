f4974c16d3186ad0d43b6fcab25f733b13691aad
Babatunde Adeyemi <tundewrites@gmail.com>
2021-02-26 12:44:55 +0100

    Remove unneccessary concurrency

--- cmd/workflow-trigger/main.go
+++ cmd/workflow-trigger/main.go
@@ -8,41 +8,32 @@ import (
 	"github.com/codingpop/workflow-trigger"
 )
 
-var (
-	owner       string
-	repo        string
-	accessToken string
-	eventType   string
-)
-
-func init() {
-	flag.StringVar(&owner, "owner", "", "Repository owner")
-	flag.StringVar(&repo, "repo", "", "GitHub repository name")
-	flag.StringVar(&accessToken, "access-token", "", "Personal access token")
-	flag.StringVar(&eventType, "event-type", "", "A custom webhook event name")
-}
+var owner = flag.String("owner", "", "Repository owner")
+var repo = flag.String("repo", "", "GitHub repository name")
+var accessToken = flag.String("access-token", "", "Personal access token")
+var eventType = flag.String("event-type", "", "A custom webhook event name")
 
 func main() {
 	flag.Parse()
 
-	if owner == "" {
+	if *owner == "" {
 		log.Fatalln("must provide repository owner")
 	}
-	if repo == "" {
+	if *repo == "" {
 		log.Fatalln("must provide repository name")
 	}
-	if accessToken == "" {
+	if *accessToken == "" {
 		log.Fatalln("must provide personal access token")
 	}
-	if eventType == "" {
+	if *eventType == "" {
 		log.Fatalln("must provide event type")
 	}
 
 	p := workflow.Params{
-		Owner:       owner,
-		Repo:        repo,
-		AccessToken: accessToken,
-		EventType:   eventType,
+		Owner:       *owner,
+		Repo:        *repo,
+		AccessToken: *accessToken,
+		EventType:   *eventType,
 	}
 	w := workflow.Configure(p)
 
--- workflow.go
+++ workflow.go
@@ -9,8 +9,6 @@ import (
 	"net/http"
 	"net/url"
 	"time"
-
-	"golang.org/x/sync/errgroup"
 )
 
 // Workflow defines the trigger functions
@@ -50,37 +48,31 @@ func Configure(p Params) *Workflow {
 	}
 }
 
-func (w *Workflow) trigger(ctx context.Context) error {
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		b, err := json.Marshal(map[string]string{
-			"event_type": w.eventType,
-		})
-		if err != nil {
-			return fmt.Errorf("failed to marshal request body: %w", err)
-		}
-
-		body := bytes.NewReader(b)
-
-		req, err := http.NewRequestWithContext(ctx, "POST", w.api, body)
-		if err != nil {
-			return fmt.Errorf("failed to create new request: %w", err)
-		}
-
-		req.Header.Set("Accept", "application/vnd.github.v3+json")
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Authorization", fmt.Sprintf("token %s", w.accessToken))
-
-		resp, err := w.client.Do(req)
-		if err != nil {
-			return fmt.Errorf("unexpected error triggering workflow: %w", err)
-		}
-
-		return handleResponse(resp)
+func (w *Workflow) trigger(ctx context.Context) (retErr error) {
+	b, err := json.Marshal(map[string]string{
+		"event_type": w.eventType,
 	})
+	if err != nil {
+		return fmt.Errorf("failed to marshal request body: %w", err)
+	}
 
-	return eg.Wait()
+	body := bytes.NewReader(b)
+
+	req, err := http.NewRequestWithContext(ctx, "POST", w.api, body)
+	if err != nil {
+		return fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", w.accessToken))
+
+	resp, err := w.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("unexpected error triggering workflow: %w", err)
+	}
+
+	return handleResponse(resp)
 }
 
 // Trigger triggers a GitHub Action workflow
